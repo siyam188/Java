79e633d89ec12fcb1e9f40e1c3149bdfd7692f37
Siyam Mia <siyambd188@gmail.com>
2025-10-08 20:48:31 +0600

    Create PrintArrayElements.java

---
+++ PrintArrayElements.java
@@ -0,0 +1,21 @@
+package array;
+public class Array {
+
+    public static void main(String[] args) {
+        int[] number =new int[5];
+        number[0]=10;
+        number[1]=20;
+        number[2]=30;
+        number[3]=40;
+        number[4]=50;
+        System.out.println(number[0]);
+        System.out.println(number[1]);
+        System.out.println(number[2]);
+         System.out.println(number[3]);
+         System.out.println(number[4]);
+         
+   
+        
+    }
+    
+}
